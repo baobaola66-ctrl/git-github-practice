910f83c042843682ab4576585983b702e5c76103
baobaola66 <baobaola66@gmail.com>
2026-04-05 20:07:17 +0800

    更新了学习dockerfile过程中的文件

---
+++ app.py
@@ -0,0 +1,4 @@
+from colorama import Fore, Style
+from utils import greet
+
+print(Fore.GREEN + greet("Dockerfile") + Style.RESET_ALL)
---
+++ utils.py
@@ -0,0 +1,2 @@
+def greet(name):
+    return f"Hello, {name}! 你已经学会了多个文件构建。"
